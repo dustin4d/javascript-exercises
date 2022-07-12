b6f82019c59a016ee022ac758656baa2caf32ce6
Dustin4d <dustinholder14@yahoo.com>
2022-07-11 20:51:30 -0500

    Completed section

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+	let splitted = string.split("");
+	let reversed = splitted.reverse();
+	let joined = reversed.join("");
+	const finalString = joined;
+	return finalString;
 };
 
+reverseString('hello');
 // Do not edit below this line
 module.exports = reverseString;
