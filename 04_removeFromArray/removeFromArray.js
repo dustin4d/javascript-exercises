1da531af8115ecc67ad52ef8607e4747e4a13820
Dustin4d <dustinholder14@yahoo.com>
2022-07-13 15:49:09 -0500

    Finish 03

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(givenArray, givenNum) {
+    const index = givenArray.indexOf(givenNum);
+    if (index > -1) {
+        givenArray.splice(index, 1);
+    }
 };
+
+removeFromArray([1,2,3,4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
