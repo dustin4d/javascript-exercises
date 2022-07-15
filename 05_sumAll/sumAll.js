b13bbd189f45acc033a3a8e026e328586cbfdc12
Dustin4d <dustinholder14@yahoo.com>
2022-07-15 16:31:26 -0500

    Complete exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
 
+    // Swap values if min is bigger than max.
+    if(min > max) {
+        [min, max] = [max, min];
+    }
+    if (typeof(min) !== 'number' || typeof(max) !== 'number') {
+        return "ERROR";
+    }
+
+    let finalNum = ((max - min) + 1) * (min + max) / 2;
+
+    if(finalNum < 0 ) {
+        return "ERROR";
+    }
+    return finalNum;
 };
+
+sumAll(10,"90");
 
 // Do not edit below this line
 module.exports = sumAll;
