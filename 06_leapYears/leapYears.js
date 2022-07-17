d8d224b5ae9405f103b8288b602d36e4f490006c
Dustin4d <dustinholder14@yahoo.com>
2022-07-17 18:57:27 -0500

    Complete exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(givenYear) {
+    const leap = new Date(givenYear, 1, 29).getDate() === 29;
+    if (leap) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
 
 // Do not edit below this line
 module.exports = leapYears;
