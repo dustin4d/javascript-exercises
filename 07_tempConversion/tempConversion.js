7f691ebc87ccfbb6dc27990f8da09a08169b483b
Dustin4d <dustinholder14@yahoo.com>
2022-07-17 19:16:58 -0500

    Complete exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -5,8 +5,11 @@ const ftoc = function(temp) {
   return parseFloat(fixed);
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let toFahrenheit = (temp * 9 /5 + 32);
+  let rounded = Math.round(toFahrenheit * 10) / 10;
+  let fixed = rounded.toFixed(1);
+  return parseFloat(fixed)
 };
 
 // Do not edit below this line
