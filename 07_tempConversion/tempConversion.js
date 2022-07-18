92a6b57bb58e7ecebd166a20649c7491cddc417e
Dustin4d <dustinholder14@yahoo.com>
2022-07-17 19:12:50 -0500

    Working Fahrenheit to Celsius function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,5 +1,8 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let toCelsius = (temp - 32) * 5 / 9;
+  let rounded = Math.round(toCelsius * 10) / 10;
+  let fixed = rounded.toFixed(1);
+  return parseFloat(fixed);
 };
 
 const ctof = function() {
