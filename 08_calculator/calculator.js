94f4101f89e4727f7f2433694c979db0b6256c93
Dustin4d <dustinholder14@yahoo.com>
2022-07-17 21:40:04 -0500

    Complete exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,40 @@
-const add = function() {
+const add = function(add1, add2) {
+  return add1 + add2;
+};
+
+const subtract = function(sub1, sub2) {
+  return sub1 - sub2;
 	
 };
 
-const subtract = function() {
-	
+const sum = function(sumArray) {
+  return sumArray.reduce(add, 0);
 };
 
-const sum = function() {
-	
+const multiply = function(multArray) {
+  let product = 1;
+
+  for(i = 0; i < multArray.length; i++) {
+    product *= multArray[i];
+  }
+  return product;
 };
 
-const multiply = function() {
-
+const power = function(base, expo) {
+  return base ** expo;
 };
 
-const power = function() {
-	
-};
+const factorial = function(num) {
+	if (num === 0 || num === 1) {
+    return 1;
+  }
 
-const factorial = function() {
-	
-};
+  for (let i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+    return num;
+
+  };
 
 // Do not edit below this line
 module.exports = {
