6ce57f63a19078d5fc8ab6164c83c7aca4b0927b
Dustin4d <dustinholder14@yahoo.com>
2022-07-23 21:39:14 -0500

    Generates a Fibonacci sequence, but does not return correct index

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let num1 = 0;
+    let num2 = 1;
+    let next;
+    let seq = [];
 
+    for(let i = 0; i <= num; i++) {
+        next = num1 + num2;
+        num1 = num2;
+        num2 = next;
+        seq.push(num1);
+    }
+    console.log(seq);
+    return seq[num];
 };
 
+fibonacci(4);
 // Do not edit below this line
 module.exports = fibonacci;
