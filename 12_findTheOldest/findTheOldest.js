95fb96266926b5a3debd77d62fd556dc38fa09d3
Dustin4d <dustinholder14@yahoo.com>
2022-07-24 22:52:42 -0500

    Returns ages of all objects

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+        for(let i = 0; i < array.length; i++) {
+            console.log(array[i].yearOfDeath - array[i].yearOfBirth);
+        }
+    }
 
-};
+    const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
+
+    findTheOldest(people);
 
 // Do not edit below this line
 module.exports = findTheOldest;
